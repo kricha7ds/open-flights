3a6eb2eb0cd783a5005c99f533d1e7af0d5a71dc
Katherine Richards <katherine.richards@rewind.io>
2022-02-21 11:15:51 -0700

    configure page routing

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airline = () => {
+    return <div>This would be the Airlines#show view for the app.</div>
+}
+
+export default Airline
---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Airlines = () => {
+    return (<div>This would be the Airlines#index view for the app.</div>)
+}
+
+export default Airlines
