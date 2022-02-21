0bd0693aaeab641cb8f3368a75215364e2e7dbce
Katherine Richards <katherine.richards@rewind.io>
2022-02-21 11:34:28 -0700

    Implement component states for Airlines homepage.

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axois from 'axios'
+import axios from 'axios'
 
 const Airlines = () => {
-    return (<div>This would be the Airlines#index view for the app.</div>)
+    const [airlines, setAirlines] = useState([])
+
+    useEffect(() => {
+        // Get all of our airlines from the api
+        // Update airlines in our state
+
+        axios.get('/api/v1/airlines.json')
+        .then( resp => {
+            setAirlines(resp.data.data)
+        })
+        .catch( resp => console.log(resp) )
+    }, [airlines.length])
+
+    const list = airlines.map( item => {
+        return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+    })
+
+    return (
+        <div className="home">
+            <div className="header">
+                <h1>Open Flights</h1>
+                <div className="subheader">Honest, unbiased airline reviews.</div>
+            </div>
+            <div className="grid">
+                <ul>{list}</ul>
+            </div>
+        </div>
+    )
 }
 
 export default Airlines
